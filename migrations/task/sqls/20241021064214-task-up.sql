ed568e01e28656f1723b244713e3993c0c72c760
hsiao-i <eva090029@gmail.com>
2024-12-07 22:23:50 +0800

    fix: 調整標點符號

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -323,7 +323,7 @@ FROM "CREDIT_PURCHASE"
 INNER JOIN "CREDIT_PACKAGE" ON "CREDIT_PURCHASE".credit_package_id = "CREDIT_PACKAGE".id
 WHERE "CREDIT_PURCHASE".created_at >= '2024-11-01 00:00:00' 
 AND "CREDIT_PURCHASE".created_at <= '2024-11-30 23:59:59'
-GROUP BY "CREDIT_PACKAGE".name
+GROUP BY "CREDIT_PACKAGE".name;
 
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
@@ -332,7 +332,7 @@ SELECT
 	SUM(price_paid) AS 總營收
 FROM "CREDIT_PURCHASE"
 WHERE "CREDIT_PURCHASE".created_at >= '2024-11-01 00:00:00' 
-AND "CREDIT_PURCHASE".created_at <= '2024-11-30 23:59:59'
+AND "CREDIT_PURCHASE".created_at <= '2024-11-30 23:59:59';
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
@@ -342,4 +342,4 @@ SELECT
 FROM "COURSE_BOOKING"
 WHERE status != '課程已取消'
 AND "COURSE_BOOKING".created_at >= '2024-11-01 00:00:00' 
-AND "COURSE_BOOKING".created_at <= '2024-11-30 23:59:59'
+AND "COURSE_BOOKING".created_at <= '2024-11-30 23:59:59';
